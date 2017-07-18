e0bb9759b64651439f1612c1a60f88ca0e05ac81
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-07-18 14:48:57 +0100

    GameView now runs on specified surface view (allowing the use of a game_activity.xml, and image buttons which can be selected.

--- JavaAndroidGameNoFramework/app/src/main/java/example/codeclan/com/javaandroidgamenoframework/GameActivity.java
+++ JavaAndroidGameNoFramework/app/src/main/java/example/codeclan/com/javaandroidgamenoframework/GameActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 /**
  * Created by user on 18/07/2017.
@@ -12,12 +15,40 @@ import android.view.Display;
 
 public class GameActivity extends AppCompatActivity {
 
+    //    declare game view
+    private GameView gameView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        getting display object
+        Display display = getWindowManager().getDefaultDisplay();
+//        getting the screen resolution into point object
+        Point size = new Point();
+        display.getSize(size);
+//        Capture specific gameView in activity_game.
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+
+//      Construct the gameView
+        gameView = new GameView(this, size.x, size.y, surfaceView);
+//      Run the gameView.
+        gameView.run();
+    }
+
+    //    Pause the game when activity is paused.
+    @Override
+    public void onPause(){
+        super.onPause();
+        gameView.pause();
+    }
+
+    //    Run the game when the game is resumed.
+    @Override
+    public void onResume(){
+        super.onResume();
+        gameView.resume();
     }
 }
--- JavaAndroidGameNoFramework/app/src/main/java/example/codeclan/com/javaandroidgamenoframework/GameView.java
+++ JavaAndroidGameNoFramework/app/src/main/java/example/codeclan/com/javaandroidgamenoframework/GameView.java
@@ -4,30 +4,23 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
 
-/**
- * Created by user on 18/07/2017.
- */
 
 public class GameView extends SurfaceView implements Runnable{
 
     //boolean variable to track if the game is playing or not
     volatile boolean playing;
-
     //the game thread
     private Thread gameThread = null;
     //    add player
     private Player player;
-    private Button btn_up, btn_down, btn_left, btn_right;
-    private Bitmap btn_upBitmap;
-    private Bitmap btn_downBitmap;
-    private Bitmap btn_leftBitmap;
-    private Bitmap btn_rightBitmap;
     //    Add objects used for drawing.
     private Paint paint;
     private Canvas canvas;
@@ -38,41 +31,118 @@ public class GameView extends SurfaceView implements Runnable{
 
 
     //Class constructor
-    public GameView(Context context, int screenX, int screenY) {
+    public GameView(Context context, int screenX, int screenY, SurfaceView surfaceView) {
         super(context);
-//        player = new Player(context, screenX, screenY);
-//        btn_upBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.up_white);
-//        btn_downBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.down_white);
-//        btn_leftBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.left_white);
-//        btn_rightBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.right_white);
+        player = new Player(context, screenX, screenY);
+        paint = new Paint();
+//    Get surface holder from specified SurfaceView as passed in constructor
+        surfaceHolder = surfaceView.getHolder();
 
-//        btn_up = new Button(context, 75, 950, btn_upBitmap);
-//        btn_down = new Button(context, 75, 750, btn_downBitmap);
-//        btn_left = new Button(context, 1500, 950, btn_leftBitmap);
-//        btn_right = new Button(context, 1700, 950, btn_rightBitmap);
+//    Add stars to arrayList
+        int numStars = 100;
+        for (int i = 0; i < numStars; i++) {
+            Star s = new Star(screenX, screenY);
+            stars.add(s);
+        }
+    }
 
-//        paint = new Paint();
-//        surfaceHolder = getHolder();
-////        add stars to arrayList
-//        int numStars = 100;
-//        for(int i = 0; i < numStars; i++){
-//            Star s = new Star(screenX, screenY);
-//            stars.add(s);
+        @Override
+        public void run(){
+            while (playing) {
+                //to update the frame
+                update();
+                //to draw the frame
+                draw();
+                //to control
+                control();
+            }
         }
 
+//    update the co-ords of characters
+    private void update() {
+        player.update();
+//        update stars with player speed
+        for(Star s: stars){
+            s.update(player.getSpeed());
+        }
 
-
-    @Override
-    public void run() {
-//        while (playing) {
-//            //to update the frame
-//            update();
-//            //to draw the frame
-//            draw();
-//            //to control
-//            control();
-//        }
     }
+//    draw characters to canvas
+    private void draw() {
+//        check if surface is valid
+        if(surfaceHolder.getSurface().isValid()){
+//            lock the canvas
+            canvas = surfaceHolder.lockCanvas();
+//            draw canvas background colour.
+            canvas.drawColor(Color.BLACK);
+//            paint stars in white
+            paint.setColor(Color.WHITE);
+//            draw all stars
+            for(Star s: stars){
+                paint.setStrokeWidth(s.getStarWidth());
+                canvas.drawPoint(s.getX(), s.getY(), paint);
+            }
+//            Draw the player
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+//            unlock the canvas
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+//    This method will control the frames per seconds drawn.
+//    Here we are calling the delay method of Thread.
+//    And this is actually setting our frame rate to around 60fps.
+    private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        //when the game is paused
+        //setting the variable to false
+        playing = false;
+        try {
+            //stopping the thread
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume() {
+        //when the game is resumed
+        //starting the thread again
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    //Control of player sprite
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+//stop boosting when player releases button.
+                player.stopBoosting();
+                break;
+
+            case MotionEvent.ACTION_DOWN:
+//boost when screen pressed.
+                player.setBoosting();
+                break;
+        }
+        return true;
+    }
+
+
 }
+
+
 
 
